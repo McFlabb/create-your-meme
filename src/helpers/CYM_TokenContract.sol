79bb7dc924c349cfad14c5b806eb9e520a8d72bb
maul0x <501staudits@gmail.com>
2025-10-01 07:45:34 -0700

    feat: Add Contract Title Natspec Comments

--- src/helpers/CYM_TokenContract.sol
+++ src/helpers/CYM_TokenContract.sol
@@ -5,4 +5,13 @@ import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 import { ERC20Pausable } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
 
+/**
+ * @title CYM_TokenContract
+ * @author CraftMeme
+ * @dev A customizable ERC20 token contract with minting, burning, and pausable features.
+ * This contract allows the owner to mint and burn tokens based on configuration. Additionally,
+ * the contract can pause or unpause all token transfers.
+ *
+ * Inherits from OpenZeppelin's ERC20, ERC20Pausable, and Ownable contracts.
+ */
 contract CYM_TokenContract {}
