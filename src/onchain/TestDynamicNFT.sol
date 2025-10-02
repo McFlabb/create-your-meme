83ec691a706ea5d0568f7b5b027b874536d0bdcd
maul0x <501staudits@gmail.com>
2025-10-02 13:50:17 -0700

    feat: Add getNFTState External View Function Natspec Comments

--- src/onchain/TestDynamicNFT.sol
+++ src/onchain/TestDynamicNFT.sol
@@ -126,6 +126,9 @@ contract TestDynamicNFT is ERC721 {
         return string(abi.encodePacked("data:application/json;base64,", _base64Encode(bytes(json))));
     }
 
+    /**
+     * @dev Get NFT state for a token
+     */
     function getNFTState(uint256 tokenId) external view returns (NFTState memory) {
         require(_ownerOf(tokenId) != address(0), "Token does not exist");
         return nftStates[tokenId];
