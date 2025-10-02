6379c21ab8376bff3e3d59f63304e82c86a84099
maul0x <501staudits@gmail.com>
2025-10-02 11:20:41 -0700

    feat: Add Events Natspec Header

--- src/helpers/CYM_TokenContract.sol
+++ src/helpers/CYM_TokenContract.sol
@@ -41,4 +41,7 @@ contract CYM_TokenContract is ERC20, ERC20Pausable, Ownable {
     /// @notice Whether the token can be burned
     bool private canBurn;
 
+    /////////////
+    // Events //
+    ///////////
 }
