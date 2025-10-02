757302bad6547718644c922ee3a5ff98b9ed1d70
maul0x <501staudits@gmail.com>
2025-10-02 13:34:09 -0700

    feat: Add Contract Title Natspec Comments

--- src/onchain/TestDynamicNFT.sol
+++ src/onchain/TestDynamicNFT.sol
@@ -4,4 +4,8 @@ pragma solidity ^0.8.19;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
+/**
+ * @title TestDynamicNFT
+ * @dev Simplified Test NFT contract for basic functionality testing
+ */
 contract TestDynamicNFT {}
