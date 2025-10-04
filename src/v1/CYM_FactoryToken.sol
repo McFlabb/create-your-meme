c0d889e8d3696811df3bc0b5009d6b371c84b858
maul0x <501staudits@gmail.com>
2025-10-04 12:36:38 -0700

    feat: Add Contract Title Natspec Section

--- src/v1/CYM_FactoryToken.sol
+++ src/v1/CYM_FactoryToken.sol
@@ -6,4 +6,11 @@ import { CYM_TokenContract } from "../helpers/CYM_TokenContract.sol";
 //import { CYM_MultiSigContract } from "./CYM_MultiSigContract.sol";
 //import { CYM_LiquidityManager } from "./CYM_LiquidityManager.sol";
 
+/**
+ * @title CYM_FactoryToken.
+ * @notice A contract that creates new memecoin tokens and Uniswap liquidity pools for that tokens.
+ * @dev Has persistant storage, MultiSigContract has volatile storage.
+ * @dev This means past signed txs data is available in this contract for more functions
+ * after tx is executed.
+ */
 contract CYM_FactoryToken {}
