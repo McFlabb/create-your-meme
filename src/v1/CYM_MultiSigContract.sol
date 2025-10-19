989c1f96d84774cdd193b7884eba193e10a1e2a3
McFlabb <ChristianMc711@outlook.com>
2025-10-18 20:54:55 -0700

    feat: Contract Title

--- src/v1/CYM_MultiSigContract.sol
+++ src/v1/CYM_MultiSigContract.sol
@@ -7,4 +7,11 @@ import { ISP } from "@signprotocol/signprotocol-evm/src/interfaces/ISP.sol";
 import { Attestation } from "@signprotocol/signprotocol-evm/src/models/Attestation.sol";
 import { DataLocation } from "@signprotocol/signprotocol-evm/src/models/DataLocation.sol";
 
+/**
+ * @title CYM_MultiSigContract
+ * @author create-your-meme
+ * @dev A multisig contract requiring multiple signers to approve transactions, adding decentralization and security.
+ * Integrates Sign Protocol to manage on-chain signature attestations for enhanced transparency and validation.
+ * @notice Works with FactoryTokenContract for meme token creation, with signers validating transactions.
+ */
 contract CYM_MultiSigContract { }
