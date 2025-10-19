a2e84ed1f3db4c3d2026f1fef36f5c160afe98b5
McFlabb <ChristianMc711@outlook.com>
2025-10-18 20:53:18 -0700

    feat: Add Necessary Library

--- src/v1/CYM_MultiSigContract.sol
+++ src/v1/CYM_MultiSigContract.sol
@@ -1,4 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { CYM_FactoryToken } from "./CYM_FactoryToken.sol";
+import { ISP } from "@signprotocol/signprotocol-evm/src/interfaces/ISP.sol";
+import { Attestation } from "@signprotocol/signprotocol-evm/src/models/Attestation.sol";
+import { DataLocation } from "@signprotocol/signprotocol-evm/src/models/DataLocation.sol";
+
 contract CYM_MultiSigContract { }
