dd5ce727188c5834a129ad3184f8a18887e8713e
McFlabb <ChristianMc711@outlook.com>
2025-10-29 11:37:15 -0700

    feat: Add Necessary Imports For LiquidityManager Contract

--- src/v1/CYM_LiquidityManager.sol
+++ src/v1/CYM_LiquidityManager.sol
@@ -1,4 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+import { IPoolManager } from "v4-core/src/interfaces/IPoolManager.sol";
+import { IHooks } from "v4-core/src/interfaces/IHooks.sol";
+import { PoolKey } from "v4-core/src/types/PoolKey.sol";
+import { CurrencyLibrary, Currency } from "v4-core/src/types/Currency.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import { VestingContract } from "./VestingContract.sol";
+
 contract CYM_LiquidityManager { }
