a111c5e6d4deae588b8e41e4df139cae78ef9d42
McFlabb <ChristianMc711@outlook.com>
2025-10-29 11:40:36 -0700

    feat: forge fmt

--- src/v1/CYM_VestingContract.sol
+++ src/v1/CYM_VestingContract.sol
@@ -11,4 +11,4 @@ import "./CYM_LiquidityManager.sol";
  * @dev Implements a token vesting schedule to release tokens over time to beneficiaries.
  * @notice This contract allows the owner to set vesting schedules and beneficiaries to claim vested tokens over time.
  */
-contract CYM_VestingContract {}
+contract CYM_VestingContract { }
